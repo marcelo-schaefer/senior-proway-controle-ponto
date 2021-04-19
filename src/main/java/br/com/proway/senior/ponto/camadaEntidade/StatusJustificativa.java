a4fb415cc4e707fa77f27502d520bd587503b00f
Vitor <vitorperes1104@gmail.com>
2021-04-19 08:11:56 -0300

    Teste justificativa

--- src/main/java/br/com/proway/senior/ponto/camadaEntidade/StatusJustificativa.java
+++ src/main/java/br/com/proway/senior/ponto/camadaEntidade/StatusJustificativa.java
@@ -2,17 +2,8 @@ package br.com.proway.senior.ponto.camadaEntidade;
 
 public enum StatusJustificativa {
 
-		APROVADO("Aprovado"),
-		REPROVADO("Reprovado"),
-		PENDENTE("Pendente");
-	
-		private String descricao;
+		APROVADO,
+		REPROVADO,
+		PENDENTE;
 		
-		StatusJustificativa(String descricao){
-			this.descricao = descricao;
-		}
-		
-		public String getDescricao() {
-			return descricao;
-		}
 }
--- src/test/java/br/com/proway/senior/ponto/camadaLogica/JustificativaLogicaTest.java
+++ src/test/java/br/com/proway/senior/ponto/camadaLogica/JustificativaLogicaTest.java
@@ -2,11 +2,14 @@ package br.com.proway.senior.ponto.camadaLogica;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import br.com.proway.senior.ponto.camadaEntidade.Justificativa;
 import br.com.proway.senior.ponto.camadaEntidade.StatusJustificativa;
+import br.com.proway.senior.ponto.camadaEntidade.StatusJustificativaTest;
 
 public class JustificativaLogicaTest {
 
@@ -68,5 +71,31 @@ public class JustificativaLogicaTest {
 			minhaJustificativa.entidadeJustificativa.setImg("Teste imagem");
 			assertTrue(minhaJustificativa.verificaImagem(objetoJustificativa));
 		}
-	
+		
+		@Test
+		public void testConsultaJustificativa() {
+			Justificativa objetoJustificativa = new Justificativa();
+			JustificativaLogica minhaJustificativa = new JustificativaLogica(objetoJustificativa);
+
+			minhaJustificativa.entidadeJustificativa.setId(1);
+			minhaJustificativa.entidadeJustificativa.setData("16/04/2020");
+			minhaJustificativa.entidadeJustificativa.setMsg("Teste mensagem");
+			minhaJustificativa.entidadeJustificativa.setImg("Teste imagem");
+			StatusJustificativa statusTest = StatusJustificativa.PENDENTE;
+			minhaJustificativa.entidadeJustificativa.setStatus(statusTest);
+			ArrayList<Justificativa> testeArray = new ArrayList<Justificativa>();
+			testeArray.add(minhaJustificativa.entidadeJustificativa);
+			String infoIdTest = "1";
+			String infoDataTest = "16/04/2020";
+			String infoMsgTest = "Teste mensagem";
+			String infoDataImg = "Teste imagem";
+			String infoStatusTest = "PENDENTE";
+			ArrayList<String> dadosRecebidos = new ArrayList<String>();
+			dadosRecebidos.add(infoIdTest+" "+infoDataTest + " " + infoMsgTest + " " + infoDataImg +" "+infoStatusTest);
+			
+			System.out.println(objetoJustificativa.toString());
+			
+			minhaJustificativa.cadastrarJustificativa(objetoJustificativa);
+			assertEquals(dadosRecebidos, minhaJustificativa.consultaJustificativas(testeArray));
+		}
 }
