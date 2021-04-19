bbc032c596f6fd21b65a67cff10a7b1e2ff96655
Lucas Nunes <lucasnunes.ln365@gmail.com>
2021-04-19 16:28:49 -0300

    Teste notificação

--- src/main/java/br/com/proway/senior/ponto/camadaLogica/NotificacaoLogica.java
+++ src/main/java/br/com/proway/senior/ponto/camadaLogica/NotificacaoLogica.java
@@ -1,8 +1,6 @@
 package br.com.proway.senior.ponto.camadaLogica;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import br.com.proway.senior.ponto.camadaEntidade.Pessoa;
 
@@ -15,79 +13,76 @@ public class NotificacaoLogica {
 	}
 
 	/**
-	 * enviarNotificacao
-	 * 
-	 * Responsavel por enviar uma mensagem em formato de string para um usuario
+	 * <h1>Envia notificacao</h1>
+	 * </br>
+	 * Responsavel por enviar uma mensagem em formato de String para um usuario
 	 * representado por um identificador. A funcao checa a existencia do usuario e
 	 * se a mensagem atende aos requerimentos do sistema.
 	 *
-	 * @param Pessoa entidadePessa, ha pessoa que sera relacionada a notificacao
-	 * @param String mensagem, mensagem a ser enviada.
+	 * @param entidadePessa Pessoa, consultor da pessoa informada.
+	 * @param mensagem String, mensagem a ser enviada.
+	 * @param id Integer, referente ao destinatário da mensagem.
 	 * @return boolean representando o sucesso da operacao (true/false)
 	 */
-	public boolean enviarNotificacao(Pessoa entidadePessoa, String mensagem) {
-
-		if (checarDestinatario(entidadePessoa) && checarValidadeMensagem(mensagem, entidadePessoa)) {
-
+	public boolean enviarNotificacao(Pessoa entidadePessoa, String mensagem, Integer id) {
+		if (checarDestinatario(entidadePessoa, id) == true && checarValidadeMensagem(mensagem) == true) {
+			ArrayList<String> listaMensagem = new ArrayList<String>();
+			listaMensagem.add(mensagem);
+			entidadePessoa.setMensagens(listaMensagem);
 			return true;
 		}
-		System.out.println("notificaao nao enviada!");
 		return false;
 	}
-
+	
 	/**
-	 * ChecarDestinatario
+	 * <h1>Apresenta a lista de mensagens da pessoa.</h1>
+	 * </br>
+	 * Mostra todas as mensagens de uma pessoa.
 	 * 
-	 * A funcao checa a existencia do usuario no banco de dados
-	 * 
-	 * @param Pessoa entidadePessoa, identificador da pessoa a ser verificada.
-	 * @return boolean representando o sucesso da operação (true/false)
+	 * @param entidadePessoa Pessoa, consultor da pessoa informada(no caso pelo id).
+	 * @param id Integer, referente a pessoa a ser consultada.
+	 * @return ArrayList<String>, referente a lista de mensagens.
 	 */
-	public boolean checarDestinatario(Pessoa entidadePessoa) {
-		// Se o destinatario existe na lista de usuarios registrados
-
-		Integer idGet = entidadePessoa.getId();
-
-		for (Pessoa pessoa : entidadePessoa.getListaPessoa()) {
-			if (pessoa.getId().equals(idGet)) {
-				return true;
-			}
+	public ArrayList<String> consultarNotificacaoPorPessoa(Pessoa entidadePessoa, Integer id) {
+		if(checarDestinatario(entidadePessoa, id) == true) {
+			return entidadePessoa.getMensagens();
 		}
-		System.out.println("destinatario nao encontrado");
-		return false;
-
+		return null;
 	}
-
+	
 	/**
-	 * Apresenta a lisata de mensagens da pessoa
-	 * 
-	 * @param Pessoa entidadePessoa, de quem vai pegar a lista
-	 * @return ArrayList<String>, lista de mensagens
-	 */
-	public ArrayList<String> consultarNotificacaoPorPessoa(Pessoa entidadePessoa) {
-
-		return entidadePessoa.getMensagens();
-	}
-
-	/**
-	 * Checar destinatario A funcao checa se a mensagem se adequa aos requerimentos
+	 * <h1>Checar validade da mensagem</h1> 
+	 * </br>
+	 * A função checa se a mensagem se adequa aos requerimentos
 	 * do sistema. O comprimento da mensagem deve estar entre 1 e 200 caracteres, se
 	 * for aceita, a mensagem ja eh salva na lista de mensagens.
 	 * 
-	 * @param String Mensagem, mensagem a ser enviada.
-	 * @param Pessoa entidadePessoa, para saber em qual lista pessoa salvar
-	 * @return boolean representando o sucesso da operacao (true/false)
+	 * @param mensagem String, mensagem a ser enviada.
+	 * @param entidadePessoa Pessoa, para saber em qual lista pessoa salvar.
+	 * @return boolean representando o sucesso da operacao (true/false).
 	 */
-	public boolean checarValidadeMensagem(String mensagem, Pessoa entidadePessoa) {
-		// Mensagem deve ser não nula, maior que 0 e menor que MAX_MESSAGE_LENGTH
-		// caracteres.
+	public boolean checarValidadeMensagem(String mensagem) {
 		mensagem = mensagem.trim();
 		if (mensagem != null && mensagem.length() > 0 && mensagem.length() <= 200) {
-			entidadePessoa.getMensagens().add(mensagem);
 			return true;
-		} else {
-			System.out.println("Mensagem em formato invalido!");
-			return false;
+		} 
+		return false;	
+	}
+
+	/**
+	 * <h1>Checar Destinatário</h1>
+	 * </br>
+	 * A funcao checa a existencia do usuario no banco de dados.
+	 * 
+	 * @param entidadePessoa Pessoa, consultor da pessoa a ser verificada(no caso pelo id).
+	 * @param id Integer, referente a pessoa a ser consultada.
+	 * @return boolean representando o sucesso da operação (true/false)
+	 */
+	public boolean checarDestinatario(Pessoa entidadePessoa, Integer id) {
+		Integer idGet = entidadePessoa.getId();
+		if (id == idGet) {
+			return true;
 		}
+		return false;
 	}
 }
